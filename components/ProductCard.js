a8e8ce2413df8ae65d575478dc0b91abb234cbe2
Mrfoxc121 <foxc121@yahoo.co.uk>
2023-03-22 11:10:30 +0000

    product page functionality

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -12,7 +12,7 @@ const ProductCard = ({ product }) => {
         <a className="group">
             <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
                 <div className="relative group-hover:opacity-75 h-72">
-                    <Image src={url} alt={altText} layout="fill" objectFit="cover" />
+                    <Image src={url} alt={altText} layout="fill" objectFit="cover" priority />
                 </div>
             </div>
             <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
