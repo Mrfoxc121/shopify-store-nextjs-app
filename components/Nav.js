29bb96e1060cb2c8d86176f8a6c1e608bb380da9
Mrfoxc121 <foxc121@yahoo.co.uk>
2023-04-01 13:02:17 +0100

    changes to seo

--- components/Nav.js
+++ components/Nav.js
@@ -16,14 +16,14 @@ export default function Nav() {
     <header className="border-b sticky top-0 z-20 bg-white px-10">
       <div className="flex items-center justify-between max-w-6xl pt-4pb-4pb-2 mx-auto lg:m-w-sreen-xl">
         <Link legacyBehavior href="/" passHref>
-          <a href="/" className=" customFont cursor-pointer text-2xl md:text-4xl py-4 font-bold"><span className='text-rose-500'>Foxes</span>  Beauty 
+          <a className=" customFont cursor-pointer text-2xl md:text-4xl py-4 font-bold"><span className='text-rose-500'>Foxes</span>  Beauty 
             <span className="pt-1 font-bold"> Boutique</span>
           </a>
         </Link>
         <p className="hidden md:inline-block md:text-sm bg-rose-200 rounded-xl shadow-sm p-2">Call: 07985324755 NOW!!!</p>
         <div className="cart flex items-center space-x-2 text-xl">
         <HiOutlineShoppingCart/>
-        <a href="#" className="font-bold text-sm md:text-lg cursor-pointer"
+        <a className="font-bold text-sm md:text-lg cursor-pointer"
         onClick={() => setCartOpen(!cartOpen)}>
           
             Basket ({cartQuantity})
